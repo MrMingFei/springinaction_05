8f0ad14dd8b44d56644ec6502fbec810f33717a7
ZhangQiaofei <18801146258@163.com>
2019-07-21 23:08:43 +0800

    2019/7/21

--- src/main/java/spittr/config/DataConfig.java
+++ src/main/java/spittr/config/DataConfig.java
@@ -1,13 +1,20 @@
 package spittr.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 public class DataConfig {
@@ -21,7 +28,38 @@ public class DataConfig {
     }
 
     @Bean
+    public BasicDataSource Data(){
+        BasicDataSource bs = new BasicDataSource();
+        bs.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        bs.setUrl("jdbc:mysql://localhost:3306/world?useSSL=true&useUnicode=true&characterEncoding=UTF8&serverTimezone=UTC");
+        bs.setUsername("root");
+        bs.setPassword("271828");
+        return bs;
+    }
+
+    @Bean
     public JdbcOperations jdbcTemplate(DataSource dataSource){
         return new JdbcTemplate(dataSource);
+    }
+
+    public SessionFactory sessionFactoryBean(DataSource dataSource){
+        try {
+            LocalSessionFactoryBean sfb = new LocalSessionFactoryBean();
+            sfb.setDataSource(dataSource);
+            sfb.setPackagesToScan("spittr.bean");
+            Properties props = new Properties();
+            props.setProperty("dialect", "org.hibernate.dialect.MySQL8Dialect");
+            sfb.setHibernateProperties(props);
+            sfb.afterPropertiesSet();
+            SessionFactory object = sfb.getObject();
+            return object;
+        }catch (IOException e){
+            return null;
+        }
+    }
+
+    @Bean
+    public BeanPostProcessor persisttenceTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
     }
 }
